8b5dff8455d5cc92ce8bca755f536de51fbb53a6
nofamex <nsiren517@gmail.com>
2021-09-23 01:25:19 +0700

    add middleware

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,4 +1,7 @@
 import express, { Router } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
 import { authRouter } from "./routers/authRouter";
 
 export const app = express();
@@ -7,6 +10,11 @@ const v1 = Router();
 
 // Middleware goes here
 app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(cors());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
 // Routing goes here
 app.use("/api", apiRouter);
